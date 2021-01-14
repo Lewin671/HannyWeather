a69c9528a05afbf49b693d784a8bbf896ea32b5e
Lewin671 <2596736318@qq.com>
2021-01-14 22:54:54 +0800

    添加weather view model

---
+++ app/src/main/java/com/example/hannyweather/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.hannyweather.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.example.hannyweather.logic.Repository
+import com.example.hannyweather.logic.model.Location
+
+class WeatherViewModel:ViewModel() {
+    private val locationLiveData = MutableLiveData<Location>()
+
+    var locationLng = ""
+    var locationLat = ""
+    var placeName = ""
+
+    val weatherLiveData = Transformations.switchMap(locationLiveData){
+        Repository.refreshWeather(it.lng,it.lat)
+    }
+
+    fun refreshWeather(lng:String,lat:String){
+        locationLiveData.value = Location(lng,lat)
+    }
+
+}
