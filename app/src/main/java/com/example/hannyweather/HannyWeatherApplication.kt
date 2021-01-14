181035c7753050b98a3e5b557067870749a29314
Lewin671 <2596736318@qq.com>
2021-01-14 21:44:59 +0800

    添加model类

---
+++ app/src/main/java/com/example/hannyweather/HannyWeatherApplication.kt
@@ -0,0 +1,17 @@
+package com.example.hannyweather
+
+import android.app.Application
+import android.content.Context
+
+class HannyWeatherApplication : Application() {
+    companion object {
+        lateinit var context: Context
+        const val TOKEN = "F6FpeVaXQXs9T3A9"
+        const val DEBUG_TAG = "HannyWeatherDebugTag"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/com/example/hannyweather/logic/model/PlaceResponse.kt
@@ -0,0 +1,16 @@
+package com.example.hannyweather.logic.model
+
+import com.google.gson.annotations.SerializedName
+
+// 数据模型的定义是根据api的返回响应来定义的
+// api样例: responseSample.json
+
+data class PlaceResponse(val status: String, val places: List<Place>)
+
+data class Place(
+    val name: String, val location: Location,
+    @SerializedName("formatted_address") val address: String
+)
+
+// 经度和纬度
+data class Location(val lng: String, val lat: String)
