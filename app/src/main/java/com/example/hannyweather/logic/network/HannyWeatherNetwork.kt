9b1a5ea1d8d99c565f22c4567fe5a78436c36a71
Lewin671 <2596736318@qq.com>
2021-01-14 22:53:36 +0800

    添加network相关函数

--- app/src/main/java/com/example/hannyweather/logic/network/HannyWeatherNetwork.kt
+++ app/src/main/java/com/example/hannyweather/logic/network/HannyWeatherNetwork.kt
@@ -10,9 +10,16 @@ import kotlin.coroutines.suspendCoroutine
 // 网络数据源入口
 object HannyWeatherNetwork {
     private val placeService = ServiceCreator.create<PlaceService>()
+    private val weatherService = ServiceCreator.create<WeatherService>()
 
     suspend fun searchPlace(query: String) = placeService.searchPlaces(query).await()
+    suspend fun getDailyWeather(lng: String, lat: String) =
+        weatherService.getDailyWeather(lng, lat).await()
 
+    suspend fun getRealTimeWeather(lng: String, lat: String) =
+        weatherService.getRealTimeWeather(lng, lat).await()
+
+    // 创建一个挂起协程来获取数据
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
             enqueue(object : Callback<T> {
@@ -36,4 +43,5 @@ object HannyWeatherNetwork {
             })
         }
     }
+
 }
