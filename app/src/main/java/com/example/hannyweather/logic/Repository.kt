599a3484e315707ced137958cb17df8d96270700
Lewin671 <2596736318@qq.com>
2021-01-14 22:54:20 +0800

    添加天气接口

--- app/src/main/java/com/example/hannyweather/logic/Repository.kt
+++ app/src/main/java/com/example/hannyweather/logic/Repository.kt
@@ -1,25 +1,63 @@
 package com.example.hannyweather.logic
 
+import android.util.Log
 import androidx.lifecycle.liveData
-import com.example.hannyweather.logic.model.Place
+import com.example.hannyweather.HannyWeatherApplication
+import com.example.hannyweather.logic.model.Weather
 import com.example.hannyweather.logic.network.HannyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
-    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
-        val result = try {
-            val placeResponse = HannyWeatherNetwork.searchPlace(query)
-            if (placeResponse.status == "ok") {
-                val places = placeResponse.places
-                Result.success(places)
-            } else {
-                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
+        val placeResponse = HannyWeatherNetwork.searchPlace(query)
+        Log.d(HannyWeatherApplication.DEBUG_TAG,"Repository: ${placeResponse.toString()}")
+        if (placeResponse.status == "ok") {
+            val places = placeResponse.places
+            Result.success(places)
+        } else {
+            Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+        }
+    }
+
+    fun refreshWeather(lng: String, lat: String) = fire(Dispatchers.IO) {
+        coroutineScope {
+            val deferredRealTime = async {
+                HannyWeatherNetwork.getRealTimeWeather(lng, lat)
+            }
+            val deferredDaily = async {
+                HannyWeatherNetwork.getDailyWeather(lng, lat)
             }
 
-        } catch (e: Exception) {
-            Result.failure<List<Place>>(e)
+            val realTimeResponse = deferredRealTime.await()
+            val dailyResponse = deferredDaily.await()
+            if (realTimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                val weather =
+                    Weather(realTimeResponse.result.realtime, dailyResponse.result.daily)
+                Result.success(weather)
+            } else {
+                Result.failure(
+                    RuntimeException(
+                        "realtime response status is ${realTimeResponse.status}" +
+                                "daily response status is ${dailyResponse.status}"
+                    )
+                )
+            }
         }
-
-        emit(result)
     }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
+        liveData(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+            emit(result)
+        }
 }
+
+
+
